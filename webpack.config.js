e3686e76378a2bcf6312fd54ea102e7ea1562241
Emre Can Cakir <emrecan.cakir@piworks.net>
2022-08-26 23:22:16 +0300

    chore: refactor webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,12 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.css$/i, use: ["style-loader", "css-loader"] },
             { test: /\.scss$/i, use: ["style-loader", "css-loader", "sass-loader"] },
+            { test: /\.css$/i, use: ["style-loader", "css-loader"] },
         ],
     },
+    devServer: {
+        static: "./dist"
+    },
+
 };
